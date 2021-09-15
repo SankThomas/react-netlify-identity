bce5a268e196519c9d54a02915ad0672e7200815
Thomas Sankara <thomassankara74@gmail.com>
2021-09-15 09:35:05 +0300

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import { useContext } from "react"
 import "./App.css"
 import Header from "./components/Header"
 import LoggedIn from "./components/LoggedIn"
+import AuthContext from "./context/auth"
 
 function App() {
+  const { user } = useContext(AuthContext)
+
   return (
     <div className="App">
       <Header />
-      <LoggedIn />
+      {user && <LoggedIn />}
     </div>
   )
 }
