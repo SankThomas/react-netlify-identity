41b557a0b9564b415a906000066f4ce8a0ae772c
Thomas Sankara <thomassankara74@gmail.com>
2021-09-15 09:14:57 +0300

    update Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import { useContext } from "react"
 import AuthContext from "../context/auth"
 
 export default function Header() {
-  const { user, login, logout } = useContext(AuthContext)
+  const { user, login } = useContext(AuthContext)
   console.log(user)
 
   return (
